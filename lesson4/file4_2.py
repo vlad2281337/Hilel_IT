2231d6e54a78c9454491bd4031540baf9a4a60a1
Admin <bi0vladusha@gmail.com>
2025-11-14 20:40:45 +0200

    add files from lesson4

--- lesson4/file4_2.py
+++ lesson4/file4_2.py
@@ -5,7 +5,7 @@ def sum_even_index_mult_last(lst):
     even_sum = sum(lst[i] for i in range(0, len(lst), 2))
     return even_sum * lst[-1]
 
-print(sum_even_index_mult_last([0, 1, 7, 2, 4, 8]))  # 88
-print(sum_even_index_mult_last([1, 3, 5]))           # 30
-print(sum_even_index_mult_last([6]))                 # 36
-print(sum_even_index_mult_last([]))                  # 0
+print(sum_even_index_mult_last([0, 1, 7, 2, 4, 8]))
+print(sum_even_index_mult_last([1, 3, 5]))
+print(sum_even_index_mult_last([6]))
+print(sum_even_index_mult_last([]))
