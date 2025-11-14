ea70b4623176af00ab238cafb38c10f0ae1cac13
Admin <bi0vladusha@gmail.com>
2025-11-14 20:39:49 +0200

    add files from lesson4

--- lesson4/file4_2.py
+++ lesson4/file4_2.py
@@ -1,11 +1,8 @@
 def sum_even_index_mult_last(lst):
-    if not lst:          # якщо список порожній → результат 0
+    if not lst:
         return 0
 
-    # сума елементів з парними індексами
     even_sum = sum(lst[i] for i in range(0, len(lst), 2))
-
-    # множимо на останній елемент
     return even_sum * lst[-1]
 
 print(sum_even_index_mult_last([0, 1, 7, 2, 4, 8]))  # 88
