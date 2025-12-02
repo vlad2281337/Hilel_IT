1d37b729f6478ace17a2a8b0ac795cb0fc081c6b
Admin <bi0vladusha@gmail.com>
2025-12-02 19:16:42 +0200

    add files from lesson9

--- lesson9/file9_1.py
+++ lesson9/file9_1.py
@@ -17,7 +17,7 @@ assert popular_words(
     ['i', 'was', 'three', 'near']
 ) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
 
-print("OK, nad the rulust is :")
+print("OK, and the result is :")
 
 print(popular_words(
     '''When I was One I had just begun
--- lesson9/file9_2.py
+++ lesson9/file9_2.py
@@ -12,7 +12,7 @@ assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
 assert difference() == 0, 'Test4'
 
-print("OK, nad the rulust is :")
+print("OK, and the result is :")
 
 print(difference(1, 2, 3))
 print(difference(5, -5))
